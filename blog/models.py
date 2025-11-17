199ad76a26efa10db70f2d6701c77cbc866837d8
Faisal Abid <ifaisalabid1@gmail.com>
2025-11-17 20:31:43 +0530

    Change function based view to class based view and implement pagination to blog list

--- blog/models.py
+++ blog/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 from django.utils import timezone
 from django.conf import settings
+from django.urls import reverse
 
 
 class PublishedManager(models.Manager):
@@ -14,7 +15,7 @@ class Post(models.Model):
         PUBLISHED = "PB", "Published"
 
     title = models.CharField(max_length=255)
-    slug = models.SlugField(max_length=255)
+    slug = models.SlugField(max_length=255, unique_for_date="publish")
     author = models.ForeignKey(
         settings.AUTH_USER_MODEL, on_delete=models.CASCADE, related_name="blog_posts"
     )
@@ -33,3 +34,9 @@ class Post(models.Model):
 
     def __str__(self):
         return self.title
+
+    def get_absolute_url(self):
+        return reverse(
+            "blog:post_detail",
+            args=[self.publish.year, self.publish.month, self.publish.day, self.slug],
+        )
---
+++ blog/views.py
@@ -0,0 +1,38 @@
+from django.shortcuts import get_object_or_404, render
+from .models import Post
+from django.core.paginator import EmptyPage, PageNotAnInteger, Paginator
+from django.views.generic import ListView
+
+
+# def post_list(request):
+#     post_list = Post.published.all()
+#     paginator = Paginator(post_list, 3)
+#     page_number = request.GET.get("page")
+
+#     try:
+#         posts = paginator.page(page_number)
+#     except PageNotAnInteger:
+#         posts = paginator.page(1)
+#     except EmptyPage:
+#         posts = paginator.page(paginator.num_pages)
+
+#     return render(request, "blog/post/list.html", {"posts": posts})
+
+
+class PostListView(ListView):
+    queryset = Post.published.all()
+    context_object_name = "posts"
+    paginate_by = 3
+    template_name = "blog/post/list.html"
+
+
+def post_detail(request, year, month, day, post):
+    post = get_object_or_404(
+        Post,
+        status=Post.Status.PUBLISHED,
+        slug=post,
+        publish__year=year,
+        publish__month=month,
+        publish__day=day,
+    )
+    return render(request, "blog/post/detail.html", {"post": post})
